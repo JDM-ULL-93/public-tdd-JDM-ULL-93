e24e4bf1657916ab41972712b9468b9748d927c6
Javier Duque Melguizo <alu0101160337@ull.edu.es>
2019-11-19 09:57:57 +0000

    C10.2: Implementado el restante codigo de 'ListaDobleEnlazada'

--- lib/gema/ListaDobleEnlazada.rb
+++ lib/gema/ListaDobleEnlazada.rb
@@ -12,21 +12,40 @@ class ListaDobleEnlazada
 	end
 	
 	def insertList(*nodos)
-		nodoPrev = nil
 		nodos.each do |nodo|
-			@size = @size + 1
-			if !(nodo.instance_of? Node) then
-				nodo = Node.new(nodo,nil,nodoPrev)
-			end
-			if (nodoPrev != nil) then
-				nodoPrev.next = nodo
-			else
-				@head = nodo
-			end
-			nodo.prev = nodoPrev
-			nodoPrev = nodo
+			self.insert(nodo)
 		end
-		@tail = nodoPrev
+	end
+	
+	def insert(nodo)
+		@size = @size + 1
+		if !(nodo.instance_of? Node) then
+			nodo = Node.new(nodo,nil,nodoPrev)
+		end
+		if (@head == nil) then
+			@head = nodo
+		end
+		if(@tail == nil) then
+			@tail = nodo
+		else
+			nodo.prev = @tail
+			@tail.next = nodo
+			@tail = nodo
+		end
+	end
+	
+	def extract_tail()
+		nodo = @tail
+		@tail = @tail.prev
+		@tail.next = nil
+		return nodo
+	end
+	
+	def extract_head()
+		nodo = @head
+		@head = @head.next
+		@head.prev = nil
+		return nodo
 	end
 end
 #end
